f86313e335a8c6e30813f24f29c4b9901cea9be0
Your Name <your.email@example.com>
2025-09-09 00:00:18 -0300

    init

--- src/br/faesa/C1/SeriesRecursivas.java
+++ src/br/faesa/C1/SeriesRecursivas.java
@@ -1,13 +1,21 @@
 package br.faesa.C1;
 public class SeriesRecursivas {
 
-    /**
-     * Calcula a soma da série 1: S = 1/4 - 2/7 + 3/10 - 4/13 + ...
-     * A função calcula a soma dos n primeiros termos.
-     *
-     * @param n O número de termos a serem somados.
-     * @return A soma da série com n termos.
-     */
+    public static void main(String[] args) {
+        int numeroDeTermos = 4;
+
+        double resultadoSerie1 = calcularSerie1(numeroDeTermos);
+        System.out.println("Resultado da Série 1: " + resultadoSerie1);
+
+        double resultadoSerie2 = calcularSerie2(numeroDeTermos);
+        System.out.println("Resultado da Série 2: " + resultadoSerie2);
+
+        long resultadoSerie3 = calcularSerie3(numeroDeTermos);
+        System.out.println("Resultado da Série 3: " + resultadoSerie3);
+
+    }
+
+    // Calcula a soma da série 1: S = 1/4 - 2/7 + 3/10 - 4/13 + ...
     public static double calcularSerie1(int n) {
         // Caso base: se não há termos para somar, a soma é 0.
         if (n <= 0) {
@@ -25,6 +33,30 @@ public class SeriesRecursivas {
                 return termo + calcularSerie1(n - 1);
             }
 
+        }
+    }
+
+    // Calcula a soma da série 2: S = 1 + 1/6 + 1/11 + 1/16 + ...
+    public static double calcularSerie2(int n) {
+        // Caso base: se não há termos, a soma é 0.
+        if (n <= 0) {
+            return 0;
+        } else {
+            // Calcula o n-ésimo termo.
+            double termo = 1.0 / (5 * n - 4);
+            // Soma o termo atual com a soma dos (n-1) termos anteriores.
+            return termo + calcularSerie2(n - 1);
+        }
+    }
+
+    // Calcula a soma da série 3: S = 1 + 4 + 9 + 16 + ...
+    public static long calcularSerie3(int n) {
+        // Caso base: se não há termos, a soma é 0.
+        if (n <= 0) {
+            return 0;
+        } else {
+            // Soma o quadrado de n com a soma dos quadrados dos (n-1) números anteriores.
+            return (long) n * n + calcularSerie3(n - 1);
         }
     }
 }
